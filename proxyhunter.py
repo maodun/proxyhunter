75dcc65d33d70437d17538bcc7212fb4975e880b
JackieZhang <jackiedhu@163.com>
2014-04-24 10:55:01 +0800

    use httplib, faster than urllib2

--- proxyhunter.py
+++ proxyhunter.py
@@ -10,36 +10,15 @@ def encode(pa):
 
 def sendsocket(host, port):
     pass
-##    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-##    s.connect((host, port))
-##    s.settimeout(30)
-##    #postdata = "id=456&id=789&id=\\'"
-##    
-##    reqdata = "GET /php/gp.php?i%64=s%6"
-##    reqdata += "5lect%%22&ie[1=1&id=\\' HTTP/1.1234"+str(i)+"\r\n"
-##    reqdata += "Host:www.baidu.com\r\n"
-##    reqdata += "content-type:application/x-www-form-urlencoded\r\n"
-##    reqdata += "content-length:%d\r\n" % len(postdata)
-##    reqdata += "\r\n"
-##    reqdata += postdata
-##              
-##    print reqdata
-##
-##    s.sendall(reqdata)
-####    import time
-####    time.sleep(1)
-####    s.sendall(reqdata2)
-##    data = s.recv(1024)
-##    s.close()
-##    print data
-##    #print repr(data.split('\r\n')[0])
-##    print '-'*20
-##    
+
+import logging
+logging.basicConfig(filename = 'proxy.log',\
+                    level = logging.INFO,\
+                    filemode = 'w',\
+                    format = '%(message)s')
 
 
-
-
-import urllib2
+import httplib
 import random
 
 def randip():
@@ -50,7 +29,7 @@ def randip():
 ##             {'http':'http://221.2.228.202:9000/'},]
 proxylist = []
 
-fproxy = open('hide_output.txt','r')
+fproxy = open('plinput.txt','r')
 fplist = fproxy.readlines()
 fplist = list(set(fplist)) #remove dup
 for proxy in fplist:
@@ -73,25 +52,54 @@ proxylen = len(proxylist)
 class SendReq:
     head = ''
     body = ''
-    def __init__(self, url, referer, xip, cookie, proxy, timeout=30):
-        proxy_handler = urllib2.ProxyHandler(proxy)
+    def __init__(self, url, referer, xip, cookie, proxyitem, timeout=30):
+        proxy = proxyitem['http'][7:-1]
+        hc = httplib.HTTPConnection(proxy, timeout=timeout)
         
-        opener = urllib2.build_opener(proxy_handler)
+        headers = {}
+        if(referer):
+            headers['Referer'] = referer
+        else:
+            headers['Referer'] = url
+        headers['X-Forwarded-For'] = xip
+        headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1838.2 Safari/537.36'
+        if(cookie):
+            headers['Cookie'] = cookie
+        self.head = {}
+        self.body = ''
+        self.code = None
+        self.url = url
         
-        req = urllib2.Request(url)
-        req.add_header('Referer', referer)
-        req.add_header('X-Forwarded-For', xip)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1838.2 Safari/537.36')
-        req.add_header('Cookie', cookie )
-        r = opener.open(req, timeout=timeout)
-        self.head = r.info()
-        self.body = r.read()
+        hc.request('GET',url, headers = headers)
+        resp = hc.getresponse()
+        
+        self.setheads(resp.getheaders())
+        self.body = resp.read()
+        hc.close()
+        
 
+        
+##        proxy_handler = urllib2.ProxyHandler(proxy)
+##        
+##        opener = urllib2.build_opener(proxy_handler)
+##        
+##        req = urllib2.Request(url)
+##        req.add_header('Referer', referer)
+##        req.add_header('X-Forwarded-For', xip)
+##        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1838.2 Safari/537.36')
+##        req.add_header('Cookie', cookie )
+##        r = opener.open(req, timeout=timeout)
+##        self.head = r.info()
+##        self.body = r.read()
+    def setheads(self, headlist):        
+        for headline in headlist:
+            self.head[headline[0].strip().lower()] = headline[1].strip()
+            
     def gethead(self):
         return str(self.head)
     
     def getbody(self):
-        return self.body
+        return str(self.body)
         
 
 import re, time
@@ -121,10 +129,10 @@ def chinaz_ip(proxy):
     ##        print posLocRight
             lock.acquire()
             print proxy, '#,', m.group(0),  ',', chinaz[posLeft + posLocLeft + 8:posLeft + posLocLeft + posLocRight].decode('utf-8').encode('gbk'), ',', "%.2fs" % (endtime - statime)
-            #res = str(proxy) + '#,' + str(m.group(0)) + ','+ str(chinaz[posLeft + posLocLeft + 8:posLeft + posLocLeft + posLocRight].decode('utf-8').encode('gbk'))+ ','
-            #res = res + "%.2fs" % (endtime - statime)
+            res = str(proxy) + '#,' + str(m.group(0)) + ','+ str(chinaz[posLeft + posLocLeft + 8:posLeft + posLocLeft + posLocRight].decode('utf-8').encode('gbk')).replace('\n','')+ ','
+            res = res + "%.2fs" % (endtime - statime)
             #print res
-            #logging.log(1,res+'\n')
+            logging.info(res)
             #print pro
             #fresult.write(pro+'\n')
             lock.release()
